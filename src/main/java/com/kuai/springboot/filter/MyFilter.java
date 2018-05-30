4287ed97e129211c6a8f3f072d4e38685dbddaec
wangjunfeng123 <wangjunfeng@meiweishenghuo.com>
2018-05-30 16:29:38 +0800

    add filter

---
+++ src/main/java/com/kuai/springboot/filter/MyFilter.java
@@ -0,0 +1,33 @@
+package com.kuai.springboot.filter;
+
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @author wangjunfeng
+ * @version 1.0
+ * @desc The type Sale menu service.
+ * @since ${date}
+ */
+@Configuration
+public class MyFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        System.out.println("this is MyFilter,url :"+request.getRequestURI());
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
