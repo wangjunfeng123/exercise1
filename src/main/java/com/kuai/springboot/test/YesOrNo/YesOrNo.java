3c13f8f4588d661698f5cd2602527f50feaac3c5
wangjunfeng123 <wangjunfeng@meiweishenghuo.com>
2018-06-02 11:25:42 +0800

    中文排序

--- src/main/java/com/kuai/springboot/test/YesOrNo/YesOrNo.java
+++ src/main/java/com/kuai/springboot/test/YesOrNo/YesOrNo.java
@@ -17,6 +17,8 @@ public class YesOrNo {
 
     public static void main(String[] args){
         System.out.println(isPublicStaticFinal(25));
+        System.out.println(Integer.toHexString(15));
+        System.out.println(Integer.valueOf("10",16));
 
     }
 
---
+++ src/main/java/com/kuai/springboot/util/SampleChineseSort.java
@@ -0,0 +1,28 @@
+package com.kuai.springboot.util;
+
+import java.text.Collator;
+import java.util.*;
+
+/**
+ * @author wangjunfeng
+ * @version 1.0
+ * @desc  中文拼音排序
+ * @since ${date}
+ */
+public class SampleChineseSort {
+    private static final Comparator CHINA_COMPARE= Collator.getInstance(Locale.CHINA);
+
+    public static void main(String[] args){
+        List<String> list = Arrays.asList("张三","李四","王五");
+        sortList(list);
+    }
+
+    private static void sortList(List<String> list){
+        Collections.sort(list,CHINA_COMPARE);
+        for (String name:list) {
+            System.out.println(name);
+        }
+    }
+
+
+}
